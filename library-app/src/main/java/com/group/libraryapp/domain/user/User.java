b7a559768ab940f603d96e6a5554b25f90313da7
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2022-11-23 23:22:28 +0900

    유저 관련 서비스 테스트 코드 작성

--- library-app/src/main/java/com/group/libraryapp/domain/user/User.java
+++ library-app/src/main/java/com/group/libraryapp/domain/user/User.java
@@ -2,6 +2,8 @@ package com.group.libraryapp.domain.user;
 
 import com.group.libraryapp.domain.book.Book;
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -16,9 +18,11 @@ public class User {
   @GeneratedValue(strategy = IDENTITY)
   private Long id;
 
+  @NotNull
   @Column(nullable = false)
   private String name;
 
+  @Nullable
   private Integer age;
 
   @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
--- library-app/src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
+++ library-app/src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
@@ -1,16 +1,24 @@
 package com.group.libraryapp.dto.user.request;
 
-public class UserCreateRequest {
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+public class UserCreateRequest {
   private String name;
   private Integer age;
 
+  @NotNull
   public String getName() {
     return name;
   }
 
+  @Nullable
   public Integer getAge() {
     return age;
   }
 
+  public UserCreateRequest(String name, Integer age) {
+    this.name = name;
+    this.age = age;
+  }
 }
