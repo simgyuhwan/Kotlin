74ab2a17c026fbdf87f3b94e637981d497fa5a7b
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2022-12-21 22:36:46 +0900

    .

--- library-app/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -16,7 +16,6 @@ class UserService(
         val newUser = User(request.name, request.age)
         userRepository.save(newUser)
     }
-
     @Transactional(readOnly = true)
     fun getUsers() : List<UserResponse> {
 //        return userRepository.findAll()
