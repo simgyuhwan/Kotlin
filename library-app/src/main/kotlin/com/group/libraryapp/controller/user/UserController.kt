fea1aaa1b5337bd412ad0305672fc2b55472d74a
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-06-22 23:30:57 +0900

    요구사항 : 유저 대출 현황 보여주기

--- library-app/src/main/kotlin/com/group/libraryapp/controller/user/UserController.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/controller/user/UserController.kt
@@ -2,6 +2,7 @@ package com.group.libraryapp.controller.user
 
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.UserLoanHistoryResponse
 import com.group.libraryapp.dto.user.response.UserResponse
 import com.group.libraryapp.service.user.UserService
 import org.springframework.web.bind.annotation.*
@@ -31,4 +32,8 @@ class UserController(
         userService.deleteUser(name)
     }
 
+    @GetMapping("/user/load")
+    fun getUserLoanHistories(): List<UserLoanHistoryResponse> {
+        return userService.getUserLoanHistories();
+    }
 }
--- library-app/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -2,8 +2,11 @@ package com.group.libraryapp.service.user
 
 import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.BookHistoryResponse
+import com.group.libraryapp.dto.user.response.UserLoanHistoryResponse
 import com.group.libraryapp.dto.user.response.UserResponse
 import com.group.libraryapp.util.findByIdOrThrow
 import org.springframework.stereotype.Service
@@ -32,5 +35,20 @@ class UserService(
     fun deleteUser(name: String){
         val user = userRepository.findByName(name)
         userRepository.delete(user)
+    }
+
+    @Transactional(readOnly = true)
+    fun getUserLoanHistories(): List<UserLoanHistoryResponse> {
+        return userRepository.findAll().map { user ->
+            UserLoanHistoryResponse(
+                name = user.name,
+                books = user.userLoanHistories.map { history ->
+                    BookHistoryResponse(
+                        name = history.bookName,
+                        isReturn = history.status == UserLoanStatus.RETURNED
+                    )
+                }
+            )
+        }
     }
 }
