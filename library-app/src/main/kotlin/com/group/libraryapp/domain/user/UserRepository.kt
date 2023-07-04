28354908a61a759618a664c6ade1cf9e1186c272
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-07-04 19:57:23 +0900

    querydsl으로 JPQL 변환

--- library-app/src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -1,12 +1,8 @@
 package com.group.libraryapp.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import java.util.Optional
 
-interface UserRepository : JpaRepository<User, Long> {
+interface UserRepository : JpaRepository<User, Long>, UserRepositoryCustom {
     fun findByName(name: String): User?
 
-    @Query("SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.userLoanHistories")
-    fun findAllWithHistories() : List<User>
 }
