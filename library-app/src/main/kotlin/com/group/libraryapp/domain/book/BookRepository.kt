9c087769d1b8e0848b5f31312d5df365e096ebe8
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-08-22 21:31:00 +0900

    DTO로 한번에 생성할 수 있도록 Projection.constructor 적용

--- library-app/src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -1,8 +1,6 @@
 package com.group.libraryapp.domain.book
 
-import com.group.libraryapp.dto.book.response.BookStatResponse
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
 interface BookRepository : JpaRepository<Book, Long> {
 
@@ -10,6 +8,6 @@ interface BookRepository : JpaRepository<Book, Long> {
     fun findBookById(bookId: Long): Book?
     fun findBookByName(name: String): Book?
 
-    @Query("SELECT NEW com.group.libraryapp.dto.book.response.BookStatResponse(b.type, COUNT(b.id)) FROM Book b GROUP BY b.type")
-    fun getStats(): List<BookStatResponse>
+//    @Query("SELECT NEW com.group.libraryapp.dto.book.response.BookStatResponse(b.type, COUNT(b.id)) FROM Book b GROUP BY b.type")
+//    fun getStats(): List<BookStatResponse>
 }
--- library-app/src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -9,6 +9,7 @@ import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
 import com.group.libraryapp.dto.book.response.BookStatResponse
+import com.group.libraryapp.repository.book.BookQuerydslRepository
 import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -16,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class BookService(
     private val bookRepository: BookRepository,
+    private val bookQuerydslRepository: BookQuerydslRepository,
     private val userRepository: UserRepository,
     private val userLoanHistoryRepository: UserLoanHistoryRepository
 ) {
@@ -50,7 +52,7 @@ class BookService(
 
     @Transactional(readOnly = true)
     fun getBookStatistics(): List<BookStatResponse> {
-        return bookRepository.getStats()
+        return bookQuerydslRepository.getStats()
     }
 
 }
